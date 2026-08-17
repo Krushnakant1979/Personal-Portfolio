21f3b63d2d83d22e5915cd2b16f7c420162a4155
KRUSHNAKANT-RUTELE <krushnakantrutele1979@gmail.com>
2026-08-17 18:27:14 +0530

    feat: update hero image and layout position

--- client/components/home/HeroSection.js
+++ client/components/home/HeroSection.js
@@ -149,7 +149,7 @@ const HeroSection = ({ initialProfile = null }) => {
             initial={{ opacity: 0, scale: 0.95 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 1, delay: 0.3 }}
-            className="relative flex justify-center items-end w-full md:w-1/3 h-auto md:h-full pointer-events-auto opacity-100 z-10 md:z-20 md:translate-x-16 lg:translate-x-24 -mt-2 md:mt-0"
+            className="relative flex justify-center items-end w-full md:w-1/3 h-auto md:h-full pointer-events-auto opacity-100 z-10 md:z-20 md:translate-x-4 lg:translate-x-8 -mt-2 md:mt-0"
           >
              {/* Gradient overlays to blend the image perfectly into the black background */}
              <div className="absolute inset-x-0 bottom-0 h-1/3 bg-gradient-to-t from-[#050505] to-transparent z-10" />
@@ -159,7 +159,7 @@ const HeroSection = ({ initialProfile = null }) => {
              {/* Portrait */}
              <div className="relative w-[280px] h-[400px] sm:w-[350px] sm:h-[500px] md:w-[500px] md:h-[750px] mt-auto mx-auto">
                <Image 
-                 src="/profile.jpg" 
+                 src="/WhatsApp.jpg.jpeg" 
                  alt="Krushnakant Rutele" 
                  fill 
                  priority={true}
